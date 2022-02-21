8dd3f855064b031473127ac2b5b9f11a2fdefd2c
Chris Leensvaart <cleensvaart@gmail.com>
2022-02-20 23:28:19 -0600

    Clean frontend
    Angular, Bootstrap, and partial Components.

--- team-2/src/app/app.module.ts
+++ team-2/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
