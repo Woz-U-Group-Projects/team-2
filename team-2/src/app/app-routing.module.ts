11f500139aaa158a8badd82f7a97f383a1d5f110
BAWhitty <88449821+BAWhitty@users.noreply.github.com>
2022-03-01 07:06:18 -0500

    Update app-routing.module.ts

--- team-2/src/app/app-routing.module.ts
+++ team-2/src/app/app-routing.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { UserComponent } from './user/user.component';
+import { WidgetComponent } from './widget/widget.component';
 
 const routes: Routes = [];
 
