7ac66ada6fbd13b034f9e425cd6d28a3456ed88a
BAWhitty <88449821+BAWhitty@users.noreply.github.com>
2022-03-15 04:22:52 -0400

    updatedapp.js

--- Config/app.js
+++ Config/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const models = require('./models');
+const { dotenv } = require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -11,6 +12,14 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+require('dotenv').config();
+mongoose.connect(mongodb+srv,//cleensvaart:g2L^VdmzuvsJJ85Db^@truck-u.xn293.mongodb.net/TRUCK-U?retryWrites=true&w=majority”)
+  then(() => {
+    console.log('Connected to the database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  }));
 app.use((req, res, next) => {
   res.send('Hello from Express');
 });
--- team-2/src/app/login/login.component.ts
+++ team-2/src/app/login/login.component.ts
@@ -9,6 +9,7 @@ export class LoginComponent implements OnInit {
   onAddPost() {
     alert('BOOM!');
   }
+  
 
   constructor() { }
 
